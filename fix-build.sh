dc7357b16b61b47b2b631326b49fa2271f6adaf7
AnimaSoma <johnpaulschell@gmail.com>
2025-06-26 21:50:48 +0200

    Restore Vercel default build command

--- fix-build.sh
+++
@@ -1,4 +0,0 @@
-
-#!/bin/bash
-chmod +x node_modules/.bin/vite
-npm run build
--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 
 import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
-import { Card, CardContent } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
 
 function Navbar() {
   return (
