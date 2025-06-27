1bcd8871976e6414c895d3cbf7c24bea354587d7
John Paul Schell <JohnPaulSchell@gmail.com>
2025-06-27 22:37:32 +0200

    Add ISRM multi-page layout

---
+++ src/main 2.jsx
@@ -0,0 +1,21 @@
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Docs from "./pages/Docs";
+import Papers from "./pages/Papers";
+import Simulations from "./pages/Simulations";
+import About from "./pages/About";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/docs" element={<Docs />} />
+      <Route path="/papers" element={<Papers />} />
+      <Route path="/simulations" element={<Simulations />} />
+      <Route path="/about" element={<About />} />
+    </Routes>
+  </Router>
+);
