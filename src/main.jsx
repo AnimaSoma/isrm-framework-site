25583a4f8530b1c1d1d2a07073fa93253368130c
John Paul Schell <JohnPaulSchell@gmail.com>
2025-06-28 10:01:26 +0200

    Add ISRM multi-page layout

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Docs from "./pages/Docs.jsx";
+import Simulations from "./pages/Simulations.jsx";
+import Papers from "./pages/Papers.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/docs" element={<Docs />} />
+      <Route path="/simulations" element={<Simulations />} />
+      <Route path="/papers" element={<Papers />} />
+    </Routes>
+  </Router>
+);
---
+++ src/pages/Simulations.jsx
@@ -0,0 +1,5 @@
+
+import React from "react";
+export default function Simulations() {
+  return <div className="p-10 text-white">Simulations Page – More coming soon.</div>;
+}
