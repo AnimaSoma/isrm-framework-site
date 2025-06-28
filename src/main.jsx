dda2de66abb3d1783f1017a3b8775081832a442b
AnimaSoma <johnpaulschell@gmail.com>
2025-06-28 12:37:20 +0200

    Update site with latest changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import ISRMBackground from './components/ISRMBackground'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ISRMBackground />
     <App />
   </React.StrictMode>
-);
+)
